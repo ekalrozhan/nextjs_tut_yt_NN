bfee44e091d52341027e672a27d83060745fc84c
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-07-23 20:21:35 +0800

    Pages and routes

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,12 @@ export default function Home() {
   return (
     <div>
       <h1>Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum fuga
+        dolorum ipsa nulla esse delectus at ipsum fugit officia sit, cupiditate
+        aperiam alias, architecto culpa? Aliquam dolorem alias perspiciatis?
+        Rerum ab tenetur amet facere fugiat esse atque velit ullam repudiandae!
+      </p>
     </div>
   );
 }

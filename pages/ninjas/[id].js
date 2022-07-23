d91e4fb662061b2b9df6ab7e7db259a86ba8232f
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-07-24 01:03:21 +0800

    Dynamic routes part2 (getStaticPaths)

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,4 +1,18 @@
-import React from "react";
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+const paths = data.map(ninjs => {
+    return {
+        params: {id: ninja.id.toString()}
+    }
+})
+
+  return {
+   paths,
+   fallback: false
+  }
+};
 
 const Details = () => {
   return (

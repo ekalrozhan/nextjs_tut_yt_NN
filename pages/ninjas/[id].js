b2017b26c6722329bb3cf85d2e9649c938a758d5
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-07-24 01:15:41 +0800

    Fetching a single item

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -2,22 +2,35 @@ export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
 
-const paths = data.map(ninjs => {
+  const paths = data.map((ninja) => {
     return {
-        params: {id: ninja.id.toString()}
-    }
-})
+      params: { id: ninja.id.toString() },
+    };
+  });
 
   return {
-   paths,
-   fallback: false
-  }
+    paths,
+    fallback: false,
+  };
 };
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const Details = ({ ninja }) => {
   return (
     <div>
-      <h1>Details Page</h1>
+      <h1>{ninja.name}</h1>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };

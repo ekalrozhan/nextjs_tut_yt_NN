2233a32e72b0eaf866dafd889cf5ecef65778d1b
haikalrozhan <haikalrozhan1992@yahoo.com>
2022-07-23 20:36:27 +0800

    Linking between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import Footer from "../comps/Footer";
 import Navbar from "../comps/Navbar";
 import styles from "../styles/Home.module.css";
@@ -15,6 +16,9 @@ export default function Home() {
         aperiam alias, architecto culpa? Aliquam dolorem alias perspiciatis?
         Rerum ab tenetur amet facere fugiat esse atque velit ullam repudiandae!
       </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
       <Footer />
     </div>
   );
